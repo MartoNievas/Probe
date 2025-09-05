4af91ad1d8dbe4e5534bc80c17cf7fefd97096a5
Marto Nievas <tinnievas@gmail.com>
2025-09-04 21:52:15 -0300

    ejercicio 4 casi terminado

--- ASM/guia-de-asm-MartoNievas-main/src/4 - Memoria dinámica/Memoria.asm
+++ ASM/guia-de-asm-MartoNievas-main/src/4 - Memoria dinámica/Memoria.asm
@@ -1,7 +1,7 @@
 extern malloc
 extern free
 extern fprintf
-
+NULL_POINTER EQU 0
 section .data
 
 section .text
@@ -26,10 +26,10 @@ strCmp:
 
 	.ciclo
 	
-	cmp al,0
+	cmp al,NULL_POINTER
 	je fin_cadena1 ;termino la cadena 1
 
-	cmp r8b,0
+	cmp r8b,NULL_POINTER
 	je fin_cadena2 ;termino la cadena 2 
 
 	cmp al, r8b ;Comparamos ambos caracteres
@@ -61,7 +61,7 @@ strCmp:
 
 
 	fin_cadena1
-	cmp r8b, 0
+	cmp r8b, NULL_POINTER
 	je iguales ;como son iguales saltamos a iguales 
 
 	;si no son iguales entonces a b le falta terminar por lo tanto es mas grande b > a
@@ -83,20 +83,127 @@ strCmp:
 	pop rbp
 	ret
 
-; char* strClone(char* a)
+; char* strClone(char* a) el argumento se paso por rdi 
 strClone:
+	push rbp ;alineada 
+	mov rbp,rsp
+
+	push rdi ;desalineada 
+	push rdx ;alineada
+	push r9 ;desalineada
+	push r8 ;alineda
+	
+	; primero verficamos que el puntero no sea null
+	cmp rdi, 0
+	je puntero_nulo
+
+	xor r9,r9 ;contador de bytes 
+	mov r9b,[rdi] ;primer caraccter 
+
+	;ahora contamos la cantidad de chars + 1
+	contar_bytes: 
+	cmp r8b,0 ;verificamos que no se el char nulo
+	je fin_conteo
+	
+	inc r9 ;contador++
+	inc rsi ; nos movemos al siguiente char
+	mov r8b, [rdi]
+	jmp contar_bytes
+
+	fin_conteo:
+	inc r9 ;contador++ por el char nulo 
+
+	mov rsi,r9 ;para llamar a malloc 
+	call malloc ;puntero nuevo en rax
+	mov rdx, rax ;movemos el puntero a rdx para poder incrementarlo
+
+	cmp rdx,0 ;verificamos que se asgino la memoria
+	je puntero_nulo ;si no se asgino devolvemos un puntero nulo
+
+	mov rdi,[rbp - 8] ;recuperamos rdi de la pial
+
+	mov r8b,[rdi]
+	copiar_cadena:
+    mov r8b, [rdi]      ; leer carácter
+    mov [rdx], r8b      ; escribir en destino
+    inc rdi             ; avanzar source
+    inc rdx             ; avanzar destino
+    cmp r8b, 0       ; verificar si es byte nulo
+    jnz copiar_cadena   ; seguir si no es nulo
+
+    jmp fin
+
+
+
+	puntero_nulo:
+	xor rax,rax
+	jmp fin
+
+	;epilogo
+	fin:
+	pop r8
+	pop r9
+	pop rdx
+	pop rdi
+	pop rbp
+	mov rsp,rbp
+	pop rbp
 	ret
 
-; void strDelete(char* a)
+; void strDelete(char* a) puntero en rdi 
 strDelete:
+	;prolog
+	push rbp
+	mov rbp, rsp
+
+	cmp rdi,0
+	je fin1
+
+	;en rdi tenemos el puntero a liberar 
+	call free ;listo esto libera la memoria 
+
+	fin1:
+	pop rbp
+	mov rsp,rbp
+
 	ret
 
 ; void strPrint(char* a, FILE* pFile)
 strPrint:
 	ret
 
-; uint32_t strLen(char* a)
+; uint32_t strLen(char* a) puntero por rdi
 strLen:
+	;prologo 
+	push rbp
+	mov rbp,rsp
+	push r8
+	sub rsp,8
+
+	cmp rdi,0
+	je .fin
+
+	mov rbx, rdi
+	xor eax,eax ;contador de chars 
+	mov r8b, [rdi]
+	
+	.contador:
+	cmp r8b,0
+	je .epilogo 
+	
+	inc eax 
+	inc rbx
+	mov r11b,[rdi + rbx]
+
+	jmp .contador
+
+	.fin
+		xor eax,eax
+		jmp .epilogo
+
+	.epilogo:
+	mov rsp,rbp
+	pop rbp
 	ret
 
 
