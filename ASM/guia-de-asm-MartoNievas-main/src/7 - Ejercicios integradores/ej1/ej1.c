c1eb87a1f087dfb047416568d94dcf9bf01363ea
Marto Nievas <tinnievas@gmail.com>
2025-09-08 09:28:54 -0300

    ejercicio integrador 1 en C

--- ASM/guia-de-asm-MartoNievas-main/src/7 - Ejercicios integradores/ej1/ej1.c
+++ ASM/guia-de-asm-MartoNievas-main/src/7 - Ejercicios integradores/ej1/ej1.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_1A_HECHO = false;
+bool EJERCICIO_1A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -18,20 +18,44 @@ bool EJERCICIO_1A_HECHO = false;
  * Funciones a implementar:
  *   - indice_a_inventario
  */
-bool EJERCICIO_1B_HECHO = false;
+bool EJERCICIO_1B_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
  */
-bool es_indice_ordenado(item_t** inventario, uint16_t* indice, uint16_t tamanio, comparador_t comparador) {
-	return true;
+// comparador_t es un puntero a funcion la cual toma como parametro dos item_t
+// Lo que entiendo por este ejercicio es que tengo que comprar si todos los
+// elementos estan ordenador segun el comparado, entonces usando transitividad
+// si inv[0] y inv[1], inv[1] y inv[2] estan ordenados entonces
+//  inv[0] e inv[2] estan ordenados
+bool es_indice_ordenado(item_t **inventario, uint16_t *indice, uint16_t tamanio,
+                        comparador_t comparador) {
+  if (inventario == NULL || indice == NULL)
+    return false;
+  for (int i = 0; i < tamanio - 1; i++) {
+    if (!(*comparador)(inventario[indice[i]], inventario[indice[i + 1]])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
  * OPCIONAL: implementar en C
  */
-item_t** indice_a_inventario(item_t** inventario, uint16_t* indice, uint16_t tamanio) {
-	// ¿Cuánta memoria hay que pedir para el resultado?
-	item_t** resultado;
-	return resultado;
+item_t **indice_a_inventario(item_t **inventario, uint16_t *indice,
+                             uint16_t tamanio) {
+  // ¿Cuánta memoria hay que pedir para el resultado?
+  // si no me equivoco malloc(sizeof(item_t)*tamanio)
+  if (inventario == NULL || indice == NULL)
+    return NULL;
+
+  item_t **resultado = malloc(sizeof(item_t) * tamanio);
+  for (int i = 0; i < tamanio; i++) {
+    item_t *curr = inventario[indice[i]];
+
+    resultado[i] = curr;
+  }
+
+  return resultado;
 }
